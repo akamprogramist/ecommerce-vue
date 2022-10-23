2f71204c62910e2db3764f31e207d60eda3b8761
Akam Programist <akadev404@gmail.com>
2022-10-23 17:13:08 +0300

    finished

--- src/components/form/formCategory.vue
+++ src/components/form/formCategory.vue
@@ -25,61 +25,6 @@ const props = defineProps({
   cats: { type: String, default: "", required: true },
 });
 </script>
-
-<!-- <template>
-  <label class="label">Category</label>
-  <div class="control mb-3">
-    <label class="radio">
-      <input
-        type="radio"
-        v-model="modelValue"
-        @input="$emit('update:modelValue', modelValue)"
-        ref="picked"
-        value="cars"
-        id="cars"
-        name="answer"
-      />
-      Cars
-    </label>
-    <label class="radio">
-      <input
-        type="radio"
-        v-model="modelValue"
-        @input="$emit('update:modelValue', modelValue)"
-        ref="picked"
-        value="clothes"
-        id="clothes"
-        name="answer"
-      />
-      Clothes
-    </label>
-    <label class="radio">
-      <input
-        type="radio"
-        v-model="modelValue"
-        @input="$emit('update:modelValue', modelValue)"
-        ref="picked"
-        value="electronics"
-        id="electronics"
-        name="answer"
-      />
-      Electronics
-    </label>
-  </div>
-</template>
-
-<script setup>
-import { ref } from "vue";
-const props = defineProps({
-  modelValue: {
-    type: String,
-    required: true,
-  },
-});
-const emit = defineEmits(["update:modelValue"]);
-const picked = ref(null);
-</script> -->
-
 <style scoped>
 .wrapper {
   display: block;
